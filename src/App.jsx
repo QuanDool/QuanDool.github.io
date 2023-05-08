a40e5126e438fe16849358f84c1e538543e2b3e5
Quan Pham <quandool0104@gmail.com>
2023-05-08 00:54:47 -0500

    add tailwindcss auto class sort plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-function App() {
+import NavBar from "./components/NavBar";
+
+export default function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<NavBar />
 		</>
 	);
 }
-
-export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+	return (
+		<nav className="flex border-2 border-black py-3 md:px-12">
+			<a className="cursor-pointer">
+				<h3 className="text-2xl font-bold">Quan Pham</h3>
+			</a>
+		</nav>
+	);
+}
