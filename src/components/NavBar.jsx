23a8ca23b918abde99fab0e86bda5154f113c218
Quan Pham <quandool0104@gmail.com>
2023-05-08 01:22:20 -0500

    style NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,29 @@
 export default function NavBar() {
 	return (
-		<nav className="flex border-2 border-black py-3 md:px-12">
+		<nav className="flex justify-between bg-slate-200 px-6 py-3 sm:px-12 md:px-24 lg:px-36 xl:px-60">
 			<a className="cursor-pointer">
 				<h3 className="text-2xl font-bold">Quan Pham</h3>
 			</a>
+			<div className="flex items-center justify-evenly gap-6 font-bold">
+				<a
+					className="transition-colors duration-300 hover:text-gray-500"
+					href=""
+				>
+					Home
+				</a>
+				<a
+					className="transition-colors duration-300 hover:text-gray-500"
+					href=""
+				>
+					Work
+				</a>
+				<a
+					className="transition-colors duration-300 hover:text-gray-500"
+					href=""
+				>
+					About
+				</a>
+			</div>
 		</nav>
 	);
 }
